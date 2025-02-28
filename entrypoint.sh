91280ed5b51a0efc30f20c39c4217ce845cd5f4f
Kairos623 <redseat623@gmail.com>
2025-02-28 19:30:16 +0900

    Создание пользователя для render
    Это сделано из блокировки консоли в бесплатном тарифе render

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,16 @@ cd app
 python manage.py makemigrations
 python manage.py migrate
 
+# Создание суперпользователя, если его еще нет
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin")
+    print("Суперпользователь создан!")
+else:
+    print("Суперпользователь уже существует.")
+EOF
+
 # Используем 'exec', чтобы PID процесса сервера был PID контейнера
 exec python manage.py runserver 0.0.0.0:$PORT
